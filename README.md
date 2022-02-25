f9b2bfa4003a5ef5a9cddc69310b197287ce5eae
sinbi1024 <98267881+sinbi1024@users.noreply.github.com>
2022-02-25 14:49:58 +0900

    view engine

---
+++ README.md
@@ -0,0 +1,2 @@
+# node.js
+inflearn '윤지수 [Node.js 웹개발로 알아보는 백엔드 자바스크립트의 이해]'
---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+    console.log("start! express server on port 3000");
+});
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get('/sending', (req, res) => {
+    res.sendFile(__dirname + '/public/form.html');
+})
+
+app.post('/email_post', (req, res) => {
+    console.log(req.body.email);
+    res.render('email.ejs', { 'email': req.body.email });
+})
