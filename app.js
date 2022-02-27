6b55a911b79647c4a35cfc69d7f403de5b790485
sinbi1024 <98267881+sinbi1024@users.noreply.github.com>
2022-02-27 16:09:37 +0900

    mysql

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'sb700210',
+    database: 'jsman',
+    port: 3306
+});
+
+connection.connect();
 
 app.listen(3000, () => {
     console.log("start! express server on port 3000");
 });
 
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -25,6 +38,19 @@ app.post('/email_post', (req, res) => {
 });
 
 app.post('/ajax_send_email', (req, res) => {
-    var resData = {'response': 'ok', 'email': req.body.email};
-    res.json(resData);
+    var email = req.body.email;
+    var resData = {};
+
+    var query = connection.query('select name from users where email="' + email + '"', (err, rows) => {
+        if (err) throw err;
+        if (rows[0]) {
+            resData.result = "ok";
+            resData.name = rows[0].name;
+        } else {
+            resData.result = "none";
+            resData.name = "";
+        }
+
+        res.json(resData);
+    });
 });
